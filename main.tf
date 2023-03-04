facdcc9ec3357f41cb177607ab6c4bd10237f130
amadou sy <amsada93@gmail.com>
2023-03-04 14:38:40 +0100

    changes on server name

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ provider "hcloud" {
 }
 
 # Create a new Hetzner Cloud server
-resource "hcloud_server" "example" {
+resource "hcloud_server" "amadousysada_web" {
   name = "asad"
   server_type = "cx11"
-  image = "ubuntu-20.04"
+  image = "debian-11"
   ssh_keys = [
-    "<your-public-ssh-key>"
+    "fedora-public-ssh-key"
   ]
   location = "nbg1"
 }
 
 # Output the server's IP address
 output "ip_address" {
-  value = hcloud_server.example.ipv4_address
+  value = hcloud_server.amadousysada_web.ipv4_address
 }
